57a73f42122431588c8caa521fae3b6ef40bad1f
Jonas Carpay <jonascarpay@gmail.com>
2021-03-31 00:14:09 +0900

    Coroutine scaffolding

--- src/Eval.hs
+++ src/Eval.hs
@@ -9,6 +9,7 @@ module Eval where
 import Control.Monad.Except
 import Control.Monad.RWS as RWS
 import Control.Monad.Writer
+import Data.Bifunctor
 import Data.Functor.Identity
 import Data.Map (Map)
 import Data.Map qualified as M
@@ -226,3 +227,47 @@ force tid =
       pure v
     Just (Computed x) -> pure x
     Nothing -> throwError "Looking up invalid thunk?"
+
+newtype Coroutine m r = Coroutine {resume :: m (Either (Coroutine m r) r)}
+
+instance Functor m => Functor (Coroutine m) where
+  fmap f (Coroutine m) = Coroutine $ bimap (fmap f) f <$> m
+
+instance Monad m => Applicative (Coroutine m) where
+  pure = Coroutine . pure . pure
+  (<*>) = ap
+
+instance Monad m => Monad (Coroutine m) where
+  Coroutine m >>= f =
+    Coroutine $
+      m >>= \case
+        Left k -> pure $ Left (k >>= f)
+        Right r -> resume (f r)
+
+instance MonadTrans Coroutine where lift = Coroutine . fmap Right
+
+instance MonadIO m => MonadIO (Coroutine m) where liftIO = lift . liftIO
+
+suspend :: Applicative m => Coroutine m r -> Coroutine m r
+suspend = Coroutine . pure . Left
+
+runCoroutine :: Monad m => Coroutine m r -> m r
+runCoroutine (Coroutine m) = m >>= either runCoroutine pure
+
+liftP2 :: Monad m => (a -> b -> c) -> Coroutine m a -> Coroutine m b -> Coroutine m c
+liftP2 f a = par (f <$> a)
+
+liftP3 :: Monad m => (a -> b -> c -> d) -> Coroutine m a -> Coroutine m b -> Coroutine m c -> Coroutine m d
+liftP3 f a b = par (liftP2 f a b)
+
+-- | Run two computations until suspension.
+-- If either suspends, this will produce a suspended computation.
+-- The difference with normal (<*>) and (>>=) is that here, if the first computation suspends, it will still attempt to run the second computation.
+par :: Monad m => Coroutine m (a -> b) -> Coroutine m a -> Coroutine m b
+par mf ma =
+  Coroutine $
+    let go (Right f) (Right a) = Right (f a)
+        go (Right f) (Left ka) = Left (f <$> ka)
+        go (Left kf) (Right a) = Left $ ($ a) <$> kf
+        go (Left kf) (Left ka) = Left $ par kf ka
+     in go <$> resume mf <*> resume ma
