b70758d4737ad6c7dc5a986382ade14827b2a663
Jonas Carpay <jonascarpay@gmail.com>
2021-07-18 15:13:08 +0900

    builtins.listToAttrs

--- src/Eval/Builtins.hs
+++ src/Eval/Builtins.hs
@@ -8,6 +8,7 @@ module Eval.Builtins (builtins) where
 import Control.Monad.Except
 import Data.ByteString qualified as BS
 import Data.ByteString.Short qualified as BSS
+import Data.Foldable (toList)
 import Data.Map (Map)
 import Data.Map qualified as M
 import Data.Sequence qualified as Seq
@@ -21,7 +22,8 @@ builtins =
   M.fromList
     [ ("nine", VPrim $ PInt 9),
       ("length", vLength),
-      ("index", vIndex)
+      ("index", vIndex),
+      ("listToAttrs", vListToAttrs)
     ]
   where
     vLength = VClosure $ \tList ->
@@ -51,3 +53,19 @@ builtins =
                 Just t -> force t
               val -> throwError $ "builtins.index: Second argument was not a string but a " <> describeValue val
         val -> throwError $ "builtins.index: Cannot get index into a " <> describeValue val
+    vListToAttrs = VClosure $ \tList ->
+      force tList >>= \case
+        VList l -> do
+          l' <- forM l $ \tAttrs ->
+            force tAttrs >>= \case
+              VAttr attrs -> case M.lookup "key" attrs of
+                Just tKey ->
+                  force tKey >>= \case
+                    VString key -> case M.lookup "value" attrs of
+                      Just value -> pure (BSS.toShort key, value)
+                      Nothing -> throwError "builtins.listToAttrs: Attribute set did not contain field \"value\""
+                    val -> throwError $ "builtins.listToAttrs: field \"key\" was not a string but a " <> describeValue val
+                Nothing -> throwError "builtins.listToAttrs: Attribute set did not contain field \"key\""
+              val -> throwError $ "builtins.listToAttrs: List element was not an attribute set but a " <> describeValue val
+          pure $ VAttr $ M.fromList $ toList l'
+        val -> throwError $ "builtins.listToAttrs: Argument was not a list but a " <> describeValue val
