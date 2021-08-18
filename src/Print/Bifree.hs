3e64d289812a9f9bc6f46679b336475b50bdf57e
Jonas Carpay <jonascarpay@gmail.com>
2021-08-18 19:54:02 +0900

    Bifree, Bicofree

--- src/Print/Bifree.hs
+++ src/Print/Bifree.hs
@@ -1,8 +1,29 @@
+{-# LANGUAGE DeriveTraversable #-}
+
 module Print.Bifree where
 
 import Data.Bifunctor
 
 newtype Bifix flip self = Bifix (self (Bifix self flip) (Bifix flip self))
 
+data Bifree flip self a = Bipure a | Bifree (self (Bifree self flip a) (Bifree flip self a))
+
+instance (Bifunctor g, Bifunctor f) => Functor (Bifree g f) where
+  fmap fn (Bipure a) = Bipure (fn a)
+  fmap fn (Bifree f) = Bifree (bimap (fmap fn) (fmap fn) f)
+
+data Bicofree flip self a = (:<)
+  { biextract :: a,
+    biunwrap :: self (Bicofree self flip a) (Bicofree flip self a)
+  }
+
+instance (Bifunctor g, Bifunctor f) => Functor (Bicofree g f) where
+  fmap fn (a :< f) = fn a :< bimap (fmap fn) (fmap fn) f
+
 bicata :: (Bifunctor g, Bifunctor f) => (g a a -> a) -> (f a a -> a) -> Bifix g f -> a
 bicata fg ff (Bifix f) = ff $ bimap (bicata ff fg) (bicata fg ff) f
+
+toBicofree :: (Bifunctor g, Bifunctor f) => (g a a -> a) -> (f a a -> a) -> Bifix g f -> Bicofree g f a
+toBicofree fg ff (Bifix f) =
+  let f' = bimap (toBicofree ff fg) (toBicofree fg ff) f
+   in ff (bimap biextract biextract f') :< f'
--- src/Print/Render.hs
+++ src/Print/Render.hs
@@ -2,7 +2,4 @@
 
 module Print.Render where
 
-data Cofree2 flip self a = a :< self (Cofree2 self flip) (Cofree2 flip self)
-  deriving (Functor, Foldable, Traversable)
-
 -- render :: Doc ->
