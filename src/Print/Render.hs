5984849922a6d5e1816c54b227144d1e5abd7665
Jonas Carpay <jonascarpay@gmail.com>
2021-08-18 19:28:20 +0900

    Bifree

--- src/Print/Render.hs
+++ src/Print/Render.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE DeriveTraversable #-}
+
 module Print.Render where
 
--- data Cofree2 f g a =
+data Cofree2 flip self a = a :< self (Cofree2 self flip) (Cofree2 flip self)
+  deriving (Functor, Foldable, Traversable)
 
 -- render :: Doc ->
