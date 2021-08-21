c4be671be1046b05c71b2f33c777bcca6a725ac3
Jonas Carpay <jonascarpay@gmail.com>
2021-08-21 20:20:22 +0900

    Simplify alignment and spacing

--- src/Print/Printer.hs
+++ src/Print/Printer.hs
@@ -6,7 +6,6 @@
 module Print.Printer
   ( Printer,
     runPrinter,
-    space,
     newline,
     indent,
     emit,
@@ -20,26 +19,15 @@ import Data.ByteString (ByteString)
 import Data.ByteString.Builder (Builder)
 import Data.ByteString.Builder qualified as BSB
 import Data.ByteString.Lazy qualified as BSL
-import Data.Semigroup
 import Data.String
 import Lens.Micro.Platform
 
-newtype PrinterContext = PrinterContext
-  { _pcIndent :: Int
-  }
+newtype PrinterContext = PrinterContext {_pcIndent :: Int}
 
 data PrinterState = PrinterState
   { _psBuilder :: Builder,
-    _psColumn :: Int,
-    _psDelimiter :: RequestedSpacer,
-    _psFreshIndent :: Bool
+    _psColumn :: Int
   }
-
-data RequestedSpacer
-  = RequestedNone
-  | RequestedSpace
-  | RequestedNewline
-  deriving (Eq, Ord)
 
 makeLenses ''PrinterContext
 makeLenses ''PrinterState
@@ -61,16 +49,12 @@ runPrinter :: Printer -> ByteString
 runPrinter (Printer m) = BSL.toStrict . BSB.toLazyByteString . view psBuilder $ execState (runReaderT m c0) s0
   where
     c0 = PrinterContext 0
-    s0 = PrinterState mempty 0 RequestedNone True
+    s0 = PrinterState mempty 0
 
 indent :: Printer -> Printer
-indent (Printer m) = Printer $ do
-  use psFreshIndent >>= \case
-    True -> m
-    False -> local (over pcIndent (+ 2)) $ do
-      psFreshIndent .= True
-      spacer
-      m
+indent (Printer m) =
+  Printer $
+    local (over pcIndent (+ 2)) $ spacer >> m
   where
     spacer :: RawPrinter ()
     spacer = do
@@ -81,23 +65,10 @@ indent (Printer m) = Printer $ do
         EQ -> pure ()
         GT -> unPrinterT newline
 
-space :: Printer
-space = Printer $ psDelimiter %= max RequestedSpace
-
 newline :: Printer
-newline = Printer $ psDelimiter %= max RequestedNewline
-
-{-# INLINE emitSpacer #-}
-emitSpacer :: RawPrinter ()
-emitSpacer = do
-  use psDelimiter >>= \case
-    RequestedNone -> pure ()
-    RequestedSpace -> emitRaw (+ 1) " "
-    RequestedNewline -> do
-      i <- view pcIndent
-      emitRaw id "\n"
-      emitRaw (const i) $ mtimesDefault i " "
-  psDelimiter .= RequestedNone
+newline = Printer $ do
+  i <- view pcIndent
+  emitRaw (const i) ("\n" <> replicate i ' ')
 
 {-# INLINE emitRaw #-}
 emitRaw :: (Int -> Int) -> String -> RawPrinter ()
@@ -106,10 +77,7 @@ emitRaw fcol s = do
   psColumn %= fcol
 
 emit :: String -> Printer
-emit s = Printer $ do
-  psFreshIndent .= False
-  emitSpacer
-  emitRaw (+ length s) s
+emit s = Printer $ emitRaw (+ length s) s
 
 emits :: Show a => a -> Printer
 emits = emit . show
--- src/Print/Render.hs
+++ src/Print/Render.hs
@@ -19,26 +19,28 @@ type AnnDoc = Bicofree StatementF DocF Style
 render :: Doc -> ByteString
 render = runPrinter . cataBicofree renderStmt renderDoc . annotate
 
+space :: Printer
+space = emit " "
+
 renderDoc :: Style -> DocF Printer Printer -> Printer
 renderDoc Single (Parens doc) = "(" <> doc <> ")"
-renderDoc Multi (Parens doc) = newline <> "( " <> indent doc <> newline <> ")"
+renderDoc Multi (Parens doc) = "(" <> indent doc <> newline <> ")"
 renderDoc _ (Symbol sym) = emit sym
 renderDoc Single (List doc) = "[" <> fold (intersperse ", " doc) <> "]"
 renderDoc Multi (List doc) =
-  indent $
-    mconcat
-      [ "[",
-        mconcat $ intersperse (newline <> ",") (indent <$> doc),
-        newline,
-        "]"
-      ]
+  mconcat
+    [ "[",
+      mconcat $ intersperse (newline <> ",") (indent <$> doc),
+      newline,
+      "]"
+    ]
 renderDoc _ (Deref' d) = "*" <> d
 renderDoc Single (Operator op l r) = l <> space <> op <> space <> r
 renderDoc Multi (Operator op l r) = l <> newline <> op <> space <> r
 renderDoc _ (Sel h n) = h <> "." <> n
 renderDoc sty (Call' fn args) = fn <> renderDoc sty (List args)
 renderDoc Single (Prog lbl blk) = lbl <> "@{" <> blk <> "}"
-renderDoc Multi (Prog lbl blk) = indent $ lbl <> "@{" <> newline <> indent blk <> newline <> "}"
+renderDoc Multi (Prog lbl blk) = lbl <> "@{" <> newline <> indent blk <> newline <> "}"
 renderDoc Single (Cond c t f) = fold $ intersperse space ["if", c, "then", t, "else", f]
 renderDoc Multi (Cond c t f) = "if" <> space <> c <> newline <> "then" <> space <> t <> newline <> "else" <> space <> f
 
